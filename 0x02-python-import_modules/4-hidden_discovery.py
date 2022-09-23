f7d1d3753a455ce8102f4d2b466cbe19193bf8b3
CollinsNasongo <nasongocollins@gmail.com>
2022-09-23 15:31:03 +0300

    Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def print_names():
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    print_names()
