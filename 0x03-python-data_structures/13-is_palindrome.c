8c21f4d507ffd7890eeb789b08dea388b963806f
CollinsNasongo <nasongocollins@gmail.com>
2022-09-26 08:39:55 +0300

    Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if singly linked list is a palindrome
+ *
+ * head: double pointer to the list
+ *
+ * Return: 1 if true and 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+
+}
