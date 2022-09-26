d79f95ac9a601c97ed5e3b2d6018eeb28170de2c
CollinsNasongo <nasongocollins@gmail.com>
2022-09-26 09:22:18 +0300

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if (my_list is None):
-        return
-
     new_list = list(my_list)
 
-    if (idx < 0):
-        return new_list
-    elif (idx > len(new_list)):
+    if (idx < 0 or idx > len(new_list) - 1):
         return new_list
     else:
         new_list[idx] = element
