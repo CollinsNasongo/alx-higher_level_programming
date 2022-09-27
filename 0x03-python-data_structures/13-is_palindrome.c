ab98aaba85a175f6809d85dedb9dc107beb8b357
CollinsNasongo <nasongocollins@gmail.com>
2022-09-27 13:18:42 +0300

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stddef.h>
 
 /**
  * is_palindrome - checks if singly linked list is a palindrome
@@ -9,5 +10,72 @@
  */
 int is_palindrome(listint_t **head)
 {
+	int len = get_len(*head);
+	int mid = len/2;
+	int front_idx = 0;
+	int back_idx = len - 1;
 
+	while (front_idx != (mid + 1))
+	{
+		if (get_num(*head, front_idx) != get_num(*head, back_idx))
+			return (0);
+		else
+		{
+			front_idx++;
+			back_idx--;
+		}
+	}
+	return (1);
+}
+
+/**
+ * get_len - Get number of elements in a list
+ *
+ * @h: pointer to first element on list
+ *
+ * Return: Length of list
+ */
+int get_len(const listint_t *h)
+{
+	int len = 0;
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+
+/**
+ * get_num - Get the number at a given index (from 0)
+ *
+ * @h: pointer to the head
+ * @idx: the index
+ *
+ * Return: the number at the index
+ */
+int get_num(listint_t *h, int idx)
+{
+	listint_t *current = h;
+	int len = get_len(h);
+	int count = 0;
+
+	if (idx > (len - 1))
+		return (0);
+	if (idx == 0)
+		return (current->n);
+	else
+	{
+		while (h)
+		{
+			if (idx == count)
+				return (current->n);
+			else
+			{
+				count++;
+				current = current->next;
+			}
+		}
+	}
+	return (0);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -18,6 +21,8 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+int get_len(const listint_t *h);
+int get_num(listint_t *h, int idx);
 
 int is_palindrome(listint_t **head);
 
