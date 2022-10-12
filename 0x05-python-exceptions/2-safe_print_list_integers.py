1f6823401447c3606a145d93f5762cdd190857ed
CollinsNasongo <nasongocollins@gmail.com>
2022-10-12 05:01:33 +0300

    Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    i = 0
+    while True:
+        try:
+            if i < x:
+                print("{:d}".format(my_list[i]), end='')
+                i += 1
+                counter += 1
+            else:
+                print()
+                return counter
+        except:
+            i += 1
